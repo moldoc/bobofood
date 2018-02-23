95b35738b7a1ce606eed17033e7c58833646a11c
Enni <cardifftardis@gmail.com>
2018-02-23 19:40:14 +0200

    Test deployment on github

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'welcome!' });
+  res.send({ hi: 'test' });
 });
 
 // Let Heroku inject the port
