ae95723933de8ca8997722f835922054b70ad898
Unknown <cardifftardis@gmail.com>
2018-05-17 19:58:20 +0300

    Authentication action added
    Made action for defining whether a user is logged in or not. The authReducer returns the user credentials if logged in, otherwise false.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-import Header from '../components/Header';
+import Header from './Header';
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
-  render(){
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
     return (
       <div>
         <BrowserRouter>
-          <div>
-            <Header/>
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
           </div>
         </BrowserRouter>
       </div>
     );
   }
-};
+}
 
-export default App;
+export default connect(null, actions)(App);
