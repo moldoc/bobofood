1a1ad7ae04183ea46e5455f2310c883a713714e6
Unknown <cardifftardis@gmail.com>
2018-05-17 19:51:57 +0300

    Typo fixed for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <nav class="green accent-4">
-        <div class="nav-wrapper">
-          <a class="brand-logo center">Nommotime</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a>Log in with Google</a></li>
+      <nav className="green accent-4">
+        <div className="nav-wrapper">
+          <a className="brand-logo center">Nommotime</a>
+          <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <li>
+              <a>Login With Google</a>
+            </li>
           </ul>
         </div>
       </nav>
