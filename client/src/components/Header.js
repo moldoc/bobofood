c953d389773bfd179fe8d78f7960d5c664faf3e6
Unknown <cardifftardis@gmail.com>
2018-05-18 10:51:50 +0300

    Authentication via header finished
    Finished authentication flow through the header. The header now has a button for logging in or out with Google, depending on the state of auth. The server directs the user to "foods" after having logged in.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export default class Header extends Component {
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
+    console.log(this.props);
     return (
       <nav className="green accent-4">
         <div className="nav-wrapper">
           <a className="brand-logo center">Nommotime</a>
           <ul id="nav-mobile" className="left hide-on-med-and-down">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     );
   }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
